ba7493e2524ac4dfc865386e71fc427fcbc1c2a4
ianpatricck <ianpatrick499@gmail.com>
2022-03-14 13:01:04 -0300

    Administrator - create and login

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+require __DIR__ . "/web/database/Connection.php";
+
 require __DIR__ . "/web/usecases/Page.php";
 require __DIR__ . "/web/usecases/User.php";
 require __DIR__ . "/web/usecases/Auth.php";
+require __DIR__ . "/web/usecases/Admin.php";
 
 require __DIR__ . "/web/usecases/helper/helper.php";
 
@@ -12,9 +15,11 @@ use Pecee\SimpleRouter\SimpleRouter as Router;
 
 use Hill\Usecases\Page;
 use Hill\Usecases\User;
+use Hill\Usecases\Admin;
 
-$page = new Page(new League\Plates\Engine("ui/views", "tpl"));
+$page = new Page(new League\Plates\Engine("ui/views", "html"));
 $user = new User();
+$admin = new Admin();
 
 /*
  * Routing
@@ -28,6 +33,8 @@ Router::get("/register", fn() => $page->register());
 Router::get("/home", fn() => $page->home());
 Router::get("/config", fn() => $page->config());
 Router::get("/config/profile", fn() => $page->profile());
+Router::get("/admin", fn() => $page->admin());
+Router::get("/admin/dashboard", fn() => $page->adminDashboard());
 
 // User
 
@@ -35,11 +42,14 @@ Router::post("/user/register", fn() => $user->create());
 Router::post("/user/login", fn() => $user->login());
 Router::post("/user/update", fn() => $user->update());
 Router::post("/user/delete", fn() => $user->delete());
+
 Router::get("/logout", fn() => $user->logout());
 
 // Admin
 
-Router::get("/admin", fn() => $page->admin());
+Router::post("/admin/create", fn() => $admin->create());
+Router::post("/admin/login", fn() => $admin->login());
+
 
 // Start Routes
 
