1a5f65a5daa020a0b04bdcda09735ea9ce73fbf1
ianpatricck <ianpatrick499@gmail.com>
2022-04-02 18:09:12 -0300

    Read pages from database

--- web/routing/pages.php
+++ web/routing/pages.php
@@ -2,9 +2,13 @@
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
+$pages = $page->read();
+
 Router::get("/", fn() => $view->index());
-Router::get("/register", fn() => $view->register());
-Router::get("/home", fn() => $view->home());
-Router::get("/config", fn() => $view->config());
-Router::get("/config/profile", fn() => $view->profile());
+
+foreach ($pages as $route) {
+    if (method_exists($view, $route["method"])) {
+        Router::get("/{$route['name']}", fn() => call_user_func_array([$view, $route["method"]], []));
+    }
+}
 
