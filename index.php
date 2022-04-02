bea3892b28e144c9d45f31638c3db3482d58e1cc
ianpatricck <ianpatrick499@gmail.com>
2022-04-02 00:10:25 -0300

    Short code, auto import controllers and separate routes

--- index.php
+++ index.php
@@ -1,68 +1,17 @@
 <?php
 
 require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/web/autoload.php";
 
-require __DIR__ . "/web/database/Connection.php";
-
-require __DIR__ . "/web/controllers/Page.php";
-require __DIR__ . "/web/controllers/View.php";
-require __DIR__ . "/web/controllers/User.php";
-require __DIR__ . "/web/controllers/Product.php";
-require __DIR__ . "/web/controllers/Auth.php";
-require __DIR__ . "/web/controllers/Admin.php";
-
-require __DIR__ . "/web/helper.php";
+importAllFrom("web/database");
+importAllFrom("web/controllers");
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
-use Hill\Controllers\View;
-use Hill\Controllers\User;
-use Hill\Controllers\Admin;
-use Hill\Controllers\Product;
-use Hill\Controllers\Page;
+require __DIR__ . '/web/defined/instances.php';
 
-$view = new View(new League\Plates\Engine("ui/views", "php"));
-$user = new User();
-$admin = new Admin();
-$product = new Product();
-$page = new Page();
-
-/*
- * Routing
- *
- */
-
-// Page
-
-Router::get("/", fn() => $view->index());
-Router::get("/register", fn() => $view->register());
-Router::get("/home", fn() => $view->home());
-Router::get("/config", fn() => $view->config());
-Router::get("/config/profile", fn() => $view->profile());
-Router::get("/admin", fn() => $view->admin());
-Router::get("/admin/create", fn() => $view->adminCreate());
-Router::get("/admin/dashboard", fn() => $view->adminDashboard());
-
-// User
-
-Router::post("/user/register", fn() => $user->create());
-Router::post("/user/login", fn() => $user->login());
-Router::post("/user/update", fn() => $user->update());
-Router::post("/user/delete", fn() => $user->delete());
-
-Router::get("/logout", fn() => $user->logout());
-
-// Admin
-
-Router::post("/admin/create", fn() => $admin->create());
-Router::post("/admin/login", fn() => $admin->login());
-
-Router::post("/page/update", fn() => $page->update());
-
-Router::post("/product/create", fn() => $product->create());
-Router::post("/product/update", fn() => $product->update());
-Router::post("/product/delete", fn() => $product->delete());
-
-// Start Routes
+require __DIR__ . '/web/routing/pages.php';
+require __DIR__ . '/web/routing/admin.php';
+require __DIR__ . '/web/routing/user.php';
 
 Router::start();
