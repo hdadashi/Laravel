3e999a240f83c79f1839dc5494d9026b409648a2
ianpatricck <ianpatrick499@gmail.com>
2022-04-17 10:33:01 -0300

    /user/create route

--- server-side/index.php
+++ server-side/index.php
@@ -4,14 +4,18 @@ require __DIR__ . "/vendor/autoload.php";
 
 $router = new \Bramus\Router\Router();
 
-$router->post("/product/create", function () {
+$router->post("/user/create", function () {
 
+    include_once __DIR__ . "/usecases/create-user/index.php";
+    $createUserController->handle($_POST);
+});
+
+$router->post("/product/create", function () {
     include_once __DIR__ . "/usecases/create-product/index.php";
     $createProductController->handle($_POST);
 });
 
 $router->post("/product/purchase", function () {
-
     include_once __DIR__ . "/usecases/purchase-product/index.php";
     $purchaseProductController->handle($_POST);
 });
