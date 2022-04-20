e31e1b47a1da6402f403bac621cdeed9dc0d7a49
ianpatricck <ianpatrick499@gmail.com>
2022-04-20 10:31:17 -0300

    Phinx migrations

---
+++ server-side/phinx.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/");
+$dotenv->load();
+
+return
+    [
+        'paths' => [
+            'migrations' => 'database/migrations'
+        ],
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_environment' => 'production',
+            'production' => [
+                'adapter' => 'pgsql',
+                'host' => $_ENV["DB_HOST"],
+                'name' => $_ENV["DB_NAME"],
+                'user' => $_ENV["DB_USER"],
+                'pass' => $_ENV["DB_PASSWORD"],
+                'port' => '5432',
+                'charset' => 'utf8',
+            ],
+        ],
+        'version_order' => 'creation'
+    ];
