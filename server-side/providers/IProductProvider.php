4de99be73e030d0dea305e97f9bb61dc0fefe0fd
ianpatricck <ianpatrick499@gmail.com>
2022-05-10 15:52:32 -0300

    Laravel app

--- server-side/providers/IProductProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface IProductProvider {
-    public function get(?int $id): object | array;
-    public function create(array $data): void;
-    public function purchase(array $data): void;
-}
--- server-side/providers/IUserProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface IUserProvider {
-    public function create(array $data): void;
-    public function findByEmail(string $email): bool;
-    public function findByCPF(string $cpf): bool;
-}
