94e2ae312c1bc7d0149fc1ee8db8e10dde3ffcb6
ianpatricck <ianpatrick499@gmail.com>
2022-05-11 11:30:31 -0300

    Product routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::controller(PageController::class)->group(function () {
+    Route::get("/", "index");
+    Route::get("/products/{id?}", "products");
+    Route::get("/process-payment", "processPayment");
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProductController::class)->group(function () {
+    Route::post("/products/buy", "buy");
 });
