b39ec6222477b10282a9a9ea3253d76e018c8ffb
ianpatricck <ianpatrick499@gmail.com>
2022-05-14 10:50:50 -0300

    Insert six more products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,11 +15,53 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table("products")->insert([
-            "title" => "Produto teste",
+            "title" => "Notebook",
             "amount" => 1500.0,
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro saepe quisquam sit et, expedita nemo excepturi laboriosam commodi eveniet quis err!",
+            "description" => "uri laboriosam commodi eveniet quis err!",
             "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
-            "category" => "Eletronicos"
+            "category" => "Eletrônicos"
         ]);
+
+
+        DB::table("products")->insert([
+            "title" => "PC",
+            "amount" => 4500.0,
+            "description" => "veniet quis err!",
+            "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
+            "category" => "Eletrônicos"
+        ]);
+
+        DB::table("products")->insert([
+            "title" => "TV",
+            "amount" => 2500.0,
+            "description" => "dasdasdjdas dhasji ahdiadhauri laboriosam commodi eveniet quis err!",
+            "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
+            "category" => "Eletrônicos"
+        ]);
+
+        DB::table("products")->insert([
+            "title" => "Rádio",
+            "amount" => 200.0,
+            "description" => "very good radio to listen something uri laboriosam commodi eveniet quis err!",
+            "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
+            "category" => "Eletrônicos"
+        ]);
+
+        DB::table("products")->insert([
+            "title" => "Relógio",
+            "amount" => 1500.0,
+            "description" => "relogio very bom kkk uri laboriosam commodi eveniet quis err!",
+            "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
+            "category" => "Eletrônicos"
+        ]);
+
+        DB::table("products")->insert([
+            "title" => "Geladeira",
+            "amount" => 1500.0,
+            "description" => "Geladeira da nasa uri laboriosam commodi eveniet quis err!",
+            "thumbs" => json_encode(["image1" => "1245257dsd242.jpg", "image2" => "1234567890.jpg"]),
+            "category" => "Eletrodomésticos"
+        ]);
+
     }
 }
