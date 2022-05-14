2ab8b3825c6abdb4e716c85abfea9db0a23e7c23
ianpatricck <ianpatrick499@gmail.com>
2022-05-14 10:39:35 -0300

    product routes to buy

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\ProductController;
 
 Route::controller(PageController::class)->group(function () {
     Route::get("/", "index");
-    Route::get("/products/{id?}", "products");
+    Route::get("/product/{id}", "product");
     Route::get("/process-payment/{method}/{id}", "processPayment");
 });
 
 Route::controller(ProductController::class)->group(function () {
-    Route::post("/products/buy", "buyProductByCreditCard");
+    Route::post("/product/buy/pix", "buyProductByPix");
 });
+
