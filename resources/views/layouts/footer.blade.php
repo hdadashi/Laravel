32b8420456b119f08955b45f9f495d9c239ee3fc
ianpatricck <ianpatrick499@gmail.com>
2022-05-14 18:43:09 -0300

    layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <small>&copy; 2022 hill, by <a href="https://github.com/ianpatricck" target="_blank">Ian Patrick</a></small>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<header class="navbar">
+    <hgroup>
+
+    <h1>Hill</h1>
+    <h2>My Simple Virtual Store</h2>
+
+    </hgroup>
+
+    <nav class="navbar_links">
+        <ul>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Termos & privacidade</a></li>
+            <li><a href="">Contato</a></li>
+        </ul> 
+    </nav>
+
+    <nav class="navbar_search-form">
+        <form method="POST">    
+            <input type="text" placeholder="Search"/>
+            <button>Search</button>
+        </form>
+    </nav>
+
+</header> 
