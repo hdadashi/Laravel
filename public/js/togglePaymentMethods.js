18a4f2a6a68e03dcb6e10041473309df4efbcbd2
ianpatricck <ianpatrick499@gmail.com>
2022-05-14 10:38:29 -0300

    generatePixData

--- public/js/togglePaymentMethods.js
+++
@@ -1,8 +0,0 @@
-var btn_showPaymenMethods = document.getElementById("showPaymentMethodsBtn");
-var paymentMethods = document.getElementById("paymentMethods");
-
-paymentMethods.style.display = "none";
-
-btn_showPaymenMethods.addEventListener("click", () => {
-    paymentMethods.style.display = "block";
-});
