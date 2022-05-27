c8500c13ac59797e114d292a1acd7f0eeed47171
ianpatricck <ianpatrick499@gmail.com>
2022-05-26 21:20:29 -0300

    Product page structure

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,33 +5,43 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Comprar {{ $product[0]->title }} - Hill eCommerce</title>
+        <title>Comprar {{ $product[0]->title }} - Hill</title>
+
+        <link rel="stylesheet" href="{{ asset("/css/app.css") }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset("/css/header.css") }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset("/css/product.css") }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset("/css/footer.css") }}" type="text/css">
     </head>
 
     <body>
 
         @include('layouts.header')
 
-        <main>
+        <script src="https://sdk.mercadopago.com/js/v2"></script>
 
-            <h1>Comprar produto</h1>
+        <main class="product-page">
 
             <section>
 
-                    <div>
-                        <!-- IMAGE -->
+                <div class="thumb">
+                    1
+                </div>
+
+                <article>
+                    <header>    
+                        <h1>{{ $product[0]->title }}</h1>
+                        <span>R${{ $product[0]->amount }}</span>
+                    </header>
+
+                    <p>{{ $product[0]->description }}</p>
+
+                    <input type="hidden" value="{{ $preferenceId }}" id="preferenceId"/>
+                   
+                    <div class="pay-container">
+                        <div class="pay-now"></div>
                     </div>
-
-                    <article>
-                        <header>    
-                            <h1>{{ $product[0]->title }}</h1>
-                            <span>{{ $product[0]->amount }}</span>
-                        </header>
-
-                        <p>{{ $product[0]->description }}</p>
-            
-                        <a href="/process-payment/pix/{{ $product[0]->id }}">Comprar</a>
-                    </article>
+                        
+                </article>
 
             </section>
 
@@ -39,6 +49,7 @@
 
         @include('layouts.footer')
 
-    </body>
+        <script src="{{ asset('/js/checkoutPro.js') }}"></script>
 
+    </body>
 </html>
