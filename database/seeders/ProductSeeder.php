0c51ce917f85cf43a64a9f57a1b29bc2cff83ba0
ianpatricck <ianpatrick499@gmail.com>
2022-05-28 23:09:45 -0300

    Product image size description

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,7 +14,7 @@ class ProductSeeder extends Seeder
             "title" => "Notebook Intel Core i3 7th Gen 4GB RAM",
             "amount" => 2700,
             "description" => "Thank you for using my words in your work. I don't need a big house, just a two-floor condo - you could say I have lofty expectations. Logan Ipsum will loop at some point. I'm in a band that does Metallica covers with our private parts - it's called Myphallica. Petrovache. I have never known a Jack that was in good enough shape to name bodybuilding after him.",
-            "thumbs" => json_encode(["image1" => "notebook.webp"]),
+            "thumbs" => json_encode(["image1" => "notebook.webp"]), # Image size -> 500 x 340
             "category" => "Eletrônicos"
         ]);
     }
