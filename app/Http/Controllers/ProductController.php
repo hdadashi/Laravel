437a15af8c725afe22ff8b9eb663b4fecfa27a2d
ianpatricck <ianpatrick499@gmail.com>
2022-06-03 17:44:34 -0300

    Fast search method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function buy() {
-        // ..
+
+    public function search(Request $request) {
+        dd($request->search);
+    }
+
+    public function searchByKeypress(Request $request) {
+ 
+        $products = DB::table("products")
+            ->where("title", "like", "{$request->data}%")
+            ->select("id", "title", "amount", "description")
+            ->get(); 
+
+        return $products;
     }
 }
