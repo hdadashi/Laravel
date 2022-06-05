461abbd00f1db080c738716ef82d3337cc6bd4fd
ianpatricck <ianpatrick499@gmail.com>
2022-06-05 10:18:05 -0300

    implemented search method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,25 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-
     public function search(Request $request) {
-        dd($request->search);
+
+        $search = filter_var($request->search, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (empty(trim($search))) {
+            return redirect("/search")->with("products", null);
+        }
+
+        $products = DB::table("products")
+            ->where("title", "like", "{$search}%")
+            ->select("id", "title", "amount", "thumbs", "description")
+            ->get(); 
+
+        if (count($products) < 1) {
+            return redirect("/search")->with("products", null);
+        }
+
+        return redirect("/search")->with("products", $products);
+        
     }
 
     public function searchByKeypress(Request $request) {
