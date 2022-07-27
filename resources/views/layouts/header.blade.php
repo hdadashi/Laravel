709deb6af9134eb458b3e9c3e379b54f1d634b51
ianpatricck <ianpatrick499@gmail.com>
2022-07-27 11:45:01 -0300

    responsive html

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,20 +1,25 @@
 <header class="navbar">
     <hgroup>
-    <a href="/">
-        <h1>Hill</h1>
-        <h2 id="header-subtitle"></h2>
-    </a>
+        <a href="/">
+            <h1>Hill</h1>
+            <h2 id="header-subtitle"></h2>
+        </a>
     </hgroup>
 
     <nav class="navbar_links">
-        <ul>
+        
+        <i class="fa-solid fa-bars" id="menu-navbar"></i>
+        <i class="fa-solid fa-xmark" id="close-menu-navbar"></i>
+
+        <ul id="navbar-links-list">
             <li><a href="/about">Sobre</a></li>
             <li><a href="/contact">Contato</a></li>
             <li><a href="/services">Serviços</a></li>
         </ul> 
     </nav>
 
-    <nav class="navbar_search-form">
+    <nav class="navbar_search-form"> 
+
         <form method="POST" action="/product/search">
 
             @csrf
@@ -22,26 +27,27 @@
             <input type="text" placeholder="Search" name="search" id="searchField"/>
 
             <button type="submit">
-                <img src="{{ asset('/icons/search.svg') }}" alt="Search"/>
+                <i class="fa-solid fa-magnifying-glass"></i>
             </button>
         </form>
-
+ 
         <div class="results" id="results-box">
 
-        <!--
-            <article id="containerFromProductSearch">
-                <a href="" id="product-link">
-                    <h1 id="titleFromProductSearch"></h1>
-                    <span id="priceFromProductSearch"></span>
-                </a>
-            </article> 
+            <!--
+                <article id="containerFromProductSearch">
+                    <a href="" id="product-link">
+                        <h1 id="titleFromProductSearch"></h1>
+                        <span id="priceFromProductSearch"></span>
+                    </a>
+                </article> 
 
-        -->
+            -->
 
         </div>
 
     </nav>
 </header>
 
-<script src="{{ asset('/js/header-typewriter.js') }}"></script>
+<script src="{{ asset('/js/headerTypeWriter.js') }}"></script>
 <script src="{{ asset('/js/debounce.js') }}"></script>
+<script src="{{ asset('/js/toggleMenuNavbar.js') }}"></script>
